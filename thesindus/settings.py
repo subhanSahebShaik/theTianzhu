cadf4ca1c75afc1c5dc8c1db0ed751d25ed2386a
SHAIK SUBHAN SAHEB <shaiksubhansaheb609@gmail.com>
2023-05-03 22:13:04 +0530

    initial commit

--- thesindus/settings.py
+++ thesindus/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'thesindus.urls'
@@ -117,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/thetianzu/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'thetianzu', 'static')]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
