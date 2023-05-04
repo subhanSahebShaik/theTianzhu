870268b38978fcbe936e8520ecc2d929aba4365c
SUBHAN SAHEB SHAIK <122206043+subhanSahebShaik@users.noreply.github.com>
2023-05-04 08:35:35 +0530

    Update settings.py

--- thesindus/settings.py
+++ thesindus/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'https://raw.githubusercontent.com/subhanSahebShaik/theTianzhu/master/static/thetianzu/'
+STATIC_URL = 'https://cdn.jsdelivr.net/gh/subhanSahebShaik/theTianzhu@master/static/thetianzu/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
