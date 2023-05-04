8ea0218c480ff5e6f3ebd3e37077e792baa6fe7d
SUBHAN SAHEB SHAIK <122206043+subhanSahebShaik@users.noreply.github.com>
2023-05-04 06:41:10 +0530

    Update settings.py

--- thesindus/settings.py
+++ thesindus/settings.py
@@ -22,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.getenv("SECRET_KEY")
-SECRET_KEY = 'django-insecure-ox63cdi1m!&12rb7cyvz6r3dhm*u%2$bq*y_uxm2kkkhatq57_'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://tianzhu.onrender.com","*"]
+ALLOWED_HOSTS = ["tianzhu.onrender.com"]
 
 
 # Application definition
